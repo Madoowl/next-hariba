4a523d2aee52de47562ba756598108ad3fbac41d
Madowwl <emma.arlanda@viacesi.fr>
2023-03-28 16:09:49 +0200

    fix: clean front feature

--- pages/components/Boutique/Boutique.tsx
+++ pages/components/Boutique/Boutique.tsx
@@ -4,7 +4,7 @@ function Boutique() {
   return (
     <>
       <h1 className="place-content-center text-3xl my-8">Boutique</h1>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-4 gap-2 mx-16">
         <ProductCard />
         <ProductCard />
         <ProductCard />
--- pages/components/Footer/Footer.tsx
+++ pages/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className=" fixed bottom-0 w-full m-2 bg-white rounded-lg shadow dark:bg-gray-800 ">
+    <footer className=" fixed bottom-0 w-full  bg-white rounded-lg shadow dark:bg-gray-800 ">
       <div className="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
