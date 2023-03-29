56a7ef506ed5d7fb0834e83cc025d4af4370ef67
Madowwl <emma.arlanda@viacesi.fr>
2023-03-29 11:35:43 +0200

    feat: add API REST from Strapi

--- pages/components/Boutique/Boutique.tsx
+++ pages/components/Boutique/Boutique.tsx
@@ -1,15 +1,24 @@
+import { fetchAPI } from "@/lib/api-rest";
 import ProductCard from "../ProductCard/ProductCard";
 
 function Boutique() {
+  const url: string = "/products";
+
+  let products: any = [];
+
+  //get data from api
+  fetchAPI(url).then((data) => {
+    products = data;
+    console.log(products);
+  });
+
   return (
     <>
       <h1 className="place-content-center text-3xl my-8">Boutique</h1>
       <div className="grid grid-cols-4 gap-2 mx-16">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products.map((product: any) => (
+          <ProductCard key={product.id} props={product.attributes} />
+        ))}
       </div>
     </>
   );
